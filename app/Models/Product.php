4849c0d4594d905946f0185f9101788ef539a82a
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2021-08-22 23:16:13 +0600

    Migrate Table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'image',
+        'amount',
+        'price',
+        'is_active',
+
+    ];
 }
